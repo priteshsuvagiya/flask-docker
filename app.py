1e4f2d7f4c079b12cebd611cc10ab620deb63ccb
Pritesh Suvagiya <priteshsuvagiya@gmail.com>
2021-01-29 12:04:32 +0530

    WIP: Flask Project Setup

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask
-app = Flask(__name__)
+from flaskr import create_app
 
-
-@app.route('/')
-def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Initialize the database
+python3 -m flask init-db
+
+python3 app.py
